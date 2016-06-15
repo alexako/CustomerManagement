990254b4f68c559c395724364d1e27c39f3aea27
'Alex <'akosialexpo@gmail.com'>
2016-06-15 18:51:35 +0800

    Initialize new Transaction form

---
+++ CustomerRegistration/StartNewTransaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomerRegistration
+{
+    public partial class StartNewTransaction : Form
+    {
+        public StartNewTransaction()
+        {
+            InitializeComponent();
+        }
+    }
+}
